44c2d2eb7dd7b895aa1b159f9d503cad2962e4ed
Theophilus Frimpong <theophilusfrimpong17@gmail.com>
2025-02-22 17:35:18 +0000

    fixed @ import issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { Button } from './components/ui/button';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Login from "./pages/Login"
+import { Button } from "./components/ui/button";
 
 
 function App() {
@@ -13,6 +13,7 @@ function App() {
       <Route path="/" element={
         <div className='font-inter'>
           This is the Homepage
+          <Button>Add Homepage</Button>
         </div>
       } />
 
