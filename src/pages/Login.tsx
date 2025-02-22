e82135a891c324a0361532b69a70753551c7f783
Ishmael-a <abuishmaelyusif204@gmail.com>
2025-02-22 14:55:58 +0000

    fixed initialization problems

---
+++ src/pages/Login.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className="w-full min-h-screen flex">
+        <div className="w-1/2 text-center items-center h-screen">
+            This is the Login Page
+        </div>
+        <div className="w-1/2 bg-purpleaccent2 text-center items-center h-screen">
+            This is the images section
+        </div>
+    </div>
+  )
+}
+
+export default Login
