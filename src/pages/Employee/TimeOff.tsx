04adc5b1fff8012cbb4ccea46204f42dd6d9841b
Theophilus Frimpong <theophilusfrimpong17@gmail.com>
2025-02-27 19:22:20 +0000

    created success card for reusability

--- src/pages/Employee/TimeOff.tsx
+++ src/pages/Employee/TimeOff.tsx
@@ -1,6 +1,7 @@
 import { DataTable } from "@/components/common/DataTable";
 import DatePicker from "@/components/common/DatePicker";
 import CustomSelect from "@/components/common/Select";
+import SuccessCard from "@/components/common/SuccessCard";
 import TimeOffModal from "@/components/Modal";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -215,28 +216,7 @@ export default function TimeOff() {
       )}
 
       {/* Success modal for timeoff creation */}
-      {isSuccess && (
-        <section
-          className="fixed inset-0  backdrop-blur-xs  flex items-center justify-center"
-          style={{ zIndex: 100 }}
-        >
-          <div className="bg-white flex flex-col items- justify-cente border p-5 min-w-md rounded-lg space-y-6">
-            <div className="flex items-center flex-col space-y-2">
-              <img src="/successIcon.svg" />
-              <p className="font-semibold text-[#181D27] text-lg">Success!</p>
-              <p className="text-[#535862] text-sm">
-                You have successfully made a request!
-              </p>
-            </div>
-            <Button
-              className="bg-[#FFA6CD] text-rgtpink hover:bg-pink-400 transition-colors duration-300 ease-in hover:text-white cursor-pointer"
-              onClick={handleCheckNow}
-            >
-              Check now
-            </Button>
-          </div>
-        </section>
-      )}
+      {isSuccess && <SuccessCard handleClick={handleCheckNow} />}
     </main>
   );
 }
