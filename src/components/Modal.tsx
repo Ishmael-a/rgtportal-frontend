957a337892aff9273378d3f39d62a1ca1a0ccdd4
Theophilus Frimpong <theophilusfrimpong17@gmail.com>
2025-03-15 22:47:02 +0000

    merged and resolved conflicts with dev

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -36,7 +36,7 @@ export const SideFormModal = <T extends FormikValues>({
   // buttonClassName,
   formClassName,
   onSubmit,
-  submitBtnText,
+  submitBtnText = "Create",
   isSubmitting,
   back,
   backFn,
